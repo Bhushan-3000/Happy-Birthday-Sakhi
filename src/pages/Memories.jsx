d9bbf0b2fd1cae24376ecdd5c429f77784d2f4d8
Bhushan-3000 <134437390+Bhushan-3000@users.noreply.github.com>
2026-03-31 15:36:44 +0530

    Memories updated

--- src/pages/Memories.jsx
+++ src/pages/Memories.jsx
@@ -11,6 +11,7 @@ import img8 from "../assets/images/img8.png";
 import img9 from "../assets/images/img9.png";
 import img10 from "../assets/images/img10.png";
 import img11 from "../assets/images/img11.png";
+import img12 from "../assets/images/img12.png";
 
 export default function Memories() {
   const media = [
@@ -25,6 +26,7 @@ export default function Memories() {
     { type: "image", src: img9 },
     { type: "image", src: img10 },
     { type: "image", src: img11 },
+    { type: "image", src: img12 },
     // { type: "image", src: "/src/assets/images/img3.png" },
     // { type: "image", src: "/src/assets/images/img4.png" },
     // { type: "video", src: "/src/assets/images/video1.mov" },
